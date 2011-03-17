479125a52625a11f07e63292ac14f3119c5f230d
Matt Conway <wr0ngway@yahoo.com>
2011-03-17 13:09:16 -0400

    Reduce memory fragmentation by not popping/pushing from restriction queue, instaed locking/scanning and only removing the ones that can be worked on.

--- lib/resque-restriction/job.rb
+++ lib/resque-restriction/job.rb
@@ -4,36 +4,51 @@ module Resque
       alias_method :origin_reserve, :reserve
       
       def reserve(queue)
-        if queue =~ /^#{Plugins::Restriction::RESTRICTION_QUEUE_PREFIX}/
-          # If processing the restriction queue, when popping and pushing to end,
-          # we can't tell when we reach the original one, so just walk up to N items
-          # of the queue so we don't run infinitely long.
-          # After N items, a worker will give up and try another queue.
-          # This way, in aggregate, all items in a restriction queue will get processed, but
-          # multiple workers won't get tied up while processing a large queue
-          begin
-            # prevent too many workers from processing restriction queue - with large queues
-            # and many workers, end up with large memory fragmentation in redis due to all
-            # the popping/pushing
-            Resque.redis.setnx(Plugins::Restriction::SCAN_LIMIT_KEY, Plugins::Restriction::SCAN_LIMIT)
-            limit = Resque.redis.decr(Plugins::Restriction::SCAN_LIMIT_KEY)
-            return nil if limit < 0
+        if queue =~ /^#{Plugins::Restriction.restriction_queue_prefix}/
+          
+          lock_timeout = Plugins::Restriction.restriction_queue_lock_timeout
+          lock_key = "#{Plugins::Restriction.restriction_queue_lock_prefix}.#{queue}"
 
-            count = [Resque.size(queue), Plugins::Restriction::RESTRICTION_QUEUE_BATCH_SIZE].min
-            count.times do |i|
+          # acquire the lock to work on the restriction queue
+          acquired_lock = Resque.redis.setnx(lock_key, Time.now.to_i + lock_timeout + 1)
+
+          # If we don't acquire the lock, check the expiration as described
+          # at http://redis.io/commands/setnx
+          if ! acquired_lock
+            # If expiration time is in the future, then don't process.  We do process if unset
+            expiration_time = Resque.redis.get(lock_key)
+            return nil if expiration_time.to_i > Time.now.to_i
+
+            # if expiration time was in the future when we set it, then don't
+            # process as someone beat us to it  We do process if unset
+            expiration_time = Resque.redis.getset(lock_key, Time.now.to_i + lock_timeout + 1)
+            return nil if expiration_time.to_i > Time.now.to_i
+          end
+
+          begin
+
+            redis_queue = "queue:#{queue}"
+            range = Resque.redis.lrange(redis_queue, 0, Plugins::Restriction.restriction_queue_batch_size - 1)
+            range.each_with_index do |queue_entry, i|
               # For the job at the head of the queue, repush to restricition queue
               # if still restricted, otherwise we have a runnable job, so create it
               # and return
-              payload = Resque.pop(queue)
-              if payload
-                if ! constantize(payload['class']).repush(queue, *payload['args'])
+              payload = Resque.decode(queue_entry)
+              if ! constantize(payload['class']).restricted?(queue, *payload['args'])
+                removed = Resque.redis.lrem(redis_queue, 1, queue_entry).to_i
+                if removed == 1
                   return new(queue, payload)
+                else
+                  return nil
                 end
               end
+
             end
 
           ensure
-            Resque.redis.incr(Plugins::Restriction::SCAN_LIMIT_KEY)
+            # don't delete expired keys as this is a race condition if expired
+            # because it is handled above
+            Resque.redis.del(lock_key) if Resque.redis.get(lock_key).to_i > Time.now.to_i
           end
 
           return nil
--- spec/resque-restriction/job_spec.rb
+++ spec/resque-restriction/job_spec.rb
@@ -5,14 +5,14 @@ describe Resque::Job do
     Resque.redis.flushall
   end
 
-  it "should repush restriction queue when reserve" do
+  it "should pop from restriction queue when reserve for unrestricted" do
     Resque.push('restriction_normal', :class => 'OneHourRestrictionJob', :args => ['any args'])
     Resque::Job.reserve('restriction_normal').should == Resque::Job.new('restriction_normal', {'class' => 'OneHourRestrictionJob', 'args' => ['any args']})
     Resque::Job.reserve('restriction_normal').should be_nil
     Resque::Job.reserve('normal').should be_nil
   end
 
-  it "should push back to restriction queue when still restricted" do
+  it "should leave on restriction queue when still restricted" do
     Resque.redis.set(OneHourRestrictionJob.redis_key(:per_hour), -1)
     Resque.push('restriction_normal', :class => 'OneHourRestrictionJob', :args => ['any args'])
     Resque::Job.reserve('restriction_normal').should be_nil
@@ -27,12 +27,12 @@ describe Resque::Job do
     Resque::Job.reserve('restriction_normal').should be_nil
   end
 
-  it "should only push back queue_length times to restriction queue" do
+  it "should only check queue_length times to restriction queue" do
     Resque.redis.set(OneHourRestrictionJob.redis_key(:per_hour), -1)
     3.times { Resque.push('restriction_normal', :class => 'OneHourRestrictionJob', :args => ['any args']) }
     Resque.size('restriction_normal').should == 3
-    OneHourRestrictionJob.should_receive(:repush).exactly(3).times.and_return(true)
-    Resque::Job.reserve('restriction_normal')
+    OneHourRestrictionJob.should_receive(:restricted?).exactly(3).times.and_return(true)
+    Resque::Job.reserve('restriction_normal').should be_nil
   end
 
   it "should set queue on restricted job class" do
@@ -48,14 +48,6 @@ describe Resque::Job do
     worker = Resque::Worker.new("*")
     worker.work(0)
     Resque.redis.lrange("failed", 0, -1).size.should == 0
-  end
-
-  it "should track scan limit when checking restricted queues" do
-    Resque.push('restriction_normal', :class => 'OneHourRestrictionJob', :args => ['any args'])
-    Resque.redis.set(Resque::Plugins::Restriction::SCAN_LIMIT_KEY, "0")
-    Resque::Job.reserve('restriction_normal').should be_nil
-    Resque.redis.set(Resque::Plugins::Restriction::SCAN_LIMIT_KEY, "100")
-    Resque::Job.reserve('restriction_normal').should == Resque::Job.new('restriction_normal', {'class' => 'OneHourRestrictionJob', 'args' => ['any args']})
   end
 
 end
