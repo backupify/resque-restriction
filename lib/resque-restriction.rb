fb1ec45c099f183c6a3e3500885ec3be31495a0b
Matt Conway <wr0ngway@yahoo.com>
2011-04-01 17:48:47 -0400

    don't require redis-server to be running for version check

--- lib/resque-restriction.rb
+++ lib/resque-restriction.rb
@@ -2,6 +2,11 @@ require 'resque'
 require 'resque-restriction/job'
 require 'resque-restriction/restriction_job'
 
-server_ver = Resque.redis.info["redis_version"].split('.').collect{|x| x.to_i}
-unsupported_version = (server_ver <=> [2, 2, 0]) < 0
+unsupported_version = false
+begin
+  server_ver = Resque.redis.info["redis_version"].split('.').collect{|x| x.to_i}
+  unsupported_version = (server_ver <=> [2, 2, 0]) < 0
+rescue
+end
+
 raise "resque-restriction requires a redis-server version >= 2.2.0" if unsupported_version
